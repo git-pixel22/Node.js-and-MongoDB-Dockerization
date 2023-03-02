f2503ad69e4b624dbf45ea0921c265228618180d
notafk-rajat <workemail.rajat@gmail.com>
2023-03-02 12:36:54 +0530

    updated index.js

--- index.js
+++ index.js
@@ -7,10 +7,19 @@ const { MONGO_USER, MONGO_PASSWORD, MONGO_PORT, MONGO_IP } = require("./config/c
 const app = express();
 const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
 
+const connectWithRetry = () => {
 mongoose
 .connect(mongoURL)
 .then(() => console.log("successfully connected to DB"))
-.catch((e) => console.log(e));
+.catch((e) => {
+    console.log(e)
+    setTimeout(connectWithRetry, 5000);
+    });
+};
+
+connectWithRetry();
+
+
 
 
 
